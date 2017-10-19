5c4d2cd12db922a2d49dd3abcf64fffce1151a71
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-19 19:24:30 +0000

    removed a console log from char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,7 @@ const MAXCHARS = 140;
 $(function() {
   $(".new-tweet form").on("input", "textarea", function() {
     const charCounter = $(this).parent().find(".counter");
+    console.log(charCounter.text());
     const charsLeft = MAXCHARS - this.value.length;
     const color = (charsLeft < 0) ? "red" : "#244751";
     charCounter.css({
